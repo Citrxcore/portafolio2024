d516e568dfb00a8887cfaadc69db20d4a3aadc56
Citrxcore <sarahrsr02@gmail.com>
2024-06-27 19:20:42 -0600

    Portafolio actualizado

---
+++ app.js
@@ -0,0 +1,30 @@
+
+//Selecciones para barra de navegación de burger
+const burger = document.querySelector("#burger-menu");
+const ul = document.querySelector("nav ul");
+const nav = document.querySelector("nav");
+
+burger.addEventListener("click", () => {
+    ul.classList.toggle("show");
+});
+
+//cerar el menu de burger
+const navLink= document.querySelector(".nav-link");
+
+navLink.forEach((link) =>
+link.addEventListener("click", () => {
+    ul.classList.remove("show");
+})
+);
+
+// comentarios pars Java
+//Selección de desplazar hacia arriba
+const scrollUp = document.querySelector("#scroll-up");
+
+//Funcionalidad de desplazar hacia arriba
+scrollUp.addEventListener("click", () => { window.scrollTo({
+    top:0,
+    left:0,
+    behavior: "smooth",
+});
+});
